07d43765a46f3a1c99c93b3ef4c6991f309a59e2
Michelly Darquia <michelly.13darquia@gmail.com>
2024-05-08 21:33:36 -0300

    Janela e modo lento na escrita de textos

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         GUI gui = new GUI();
 
-        // Criar uma instância da classe Utils
         Utils utils = new Utils();
 
-        // Chamar o método GerarIntro() usando a instância da classe Utils
         utils.GerarIntro();
     }
 }
